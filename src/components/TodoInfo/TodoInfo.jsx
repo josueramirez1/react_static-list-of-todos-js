a8ef58396fba69f87957447b1d1af26ed3b3ddbc
Josue Ramirez <josue67ramirez@gmail.com>
2025-05-10 00:33:22 -0700

    adding changes

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -6,12 +6,12 @@ export const TodoInfo = ({ todo }) => (
     {todo.completed ? (
       <article className="TodoInfo TodoInfo--completed">
         <h2 className="TodoInfo__title">{todo.title}</h2>
-        <UserInfo user={todo.user} key={todo.user.id} />;
+        <UserInfo user={todo.user} />
       </article>
     ) : (
       <article className="TodoInfo">
         <h2 className="TodoInfo__title">{todo.title}</h2>
-        <UserInfo user={todo.user} key={todo.user.id} />;
+        <UserInfo user={todo.user} />
       </article>
     )}
   </div>
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,5 +3,9 @@ import { TodoInfo } from '../TodoInfo/TodoInfo';
 
 export const TodoList = ({ todos }) =>
   todos.map(todo => {
-    return <TodoInfo todo={todo} key={todo.id} />;
+    return (
+      <div>
+        <TodoInfo todo={todo} key={todo.id} />
+      </div>
+    );
   });
